5b53416573ac5050d79b9dc687f822bcaee4627f
phakwalun <98234845+phakwalun@users.noreply.github.com>
2022-01-31 23:07:24 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -1,8 +1,14 @@
 <html>
   <head>
-    <title>งานที่ 1</title>
+    <title>page1</title>
   </head>
-  <body>
-    page1
+  <body bgcolor="lightblue">
+    <h1 style="color=darkblue;>ประวัติส่วนตัว</h1>
+               <p><b>ชื่อ:</b> <i>นางสาวภัควลัญชญ์ บุญยัง</i></p>
+               <p><b>ชื่อเล่น:</b> <i>ซี</i></p>
+               <p><b>ชั้น:</b> <i>ม.4/12</i></p>
+               <p><b>เลขที่:</b> <i>23</i></p>
+               <p><b>เลขประจำตัวนักเรียน:</b> <i>57069</i></p>
+               <p><b>โรงเรียน:</b> <i>หล่มสักวิทยาคม</i></p>
   </body>
 </html>
