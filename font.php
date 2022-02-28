a4c70f18f874638cf2cf48947fb351ec113ea523
phakwalun <98234845+phakwalun@users.noreply.github.com>
2022-02-28 18:08:35 +0700

    Update font.php

--- font.php
+++ font.php
@@ -4,14 +4,22 @@
     <style>
       @font-face{
         font-family: "MyFont1";
-        src: url("baozi.ttf");
+        src: url("MiPancake.zip");
       }
-      body{
+      @font-face{
+        font-family: "MyFont2";
+        src: url("MNN_Tuaklom.zip");
+      }
+      #font1{
         font-family: "MyFont1";
       }
+      #font2{
+        font-family: "MyFont2"
+      }    
     </style>
   </head>
   <body>
-    <h1>ฟอนต์ของฉัน</h1>
+    <h1 id= "font1">นางสาวภัควลัญชญ์ บุญยัง ม.4/12 เลขที่ 23 เลขประจำตัว 57069</h1>
+    <h1 id= "font2">นางสาวภัควลัญชญ์ บุญยัง ม.4/12 เลขที่ 23 เลขประจำตัว 57069</h1>
   </body>
 </html>
