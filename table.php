dbe5c596d417eed1ca9791af38526aa19f3f1223
phakwalun <98234845+phakwalun@users.noreply.github.com>
2022-03-06 22:15:52 +0700

    Update table.php

--- table.php
+++ table.php
@@ -5,14 +5,40 @@
   <body>
     <table border="1">
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>FB/Twitter</th>
       </tr>
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <td>1</td>
+        <td>Leah Gardner</td>
+        <td><img src="https://pbs.twimg.com/profile_images/1431626207318183951/-Mohgx1t.jpg"></td>
+        <td><a href="https://twitter.com/leahgardnerart?t=EmGikcvGoYK1u5-2RKKWsA&s=09&fbclid=IwAR02M9_TWDbSRsUxmcONCEuEBlpAwLt0lmMNHtfQd0tM36_jNaw2uS7S0Yo">Twitter</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Ong seongwu</td>
+        <td><img src="https://i.scdn.co/image/ab67616d0000b2737c03d812d3ba04c25957160c"></td>
+        <td><a href="https://twitter.com/officialtwt_OSW?t=dthxyBeMsmUQxBQtd5NghA&s=09&fbclid=IwAR0MqAr-dmvndw9QyO7CmRM93C_cqq7ni4qnrL6AyoFwGJAarNnOuseh8KE">Twitter</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>Riku mukai</td>
+        <td><img src="https://nitter.net/pic/media%2FEzQtwRiUYAYyA7R.jpg%3Fname%3Dsmall"></td>
+        <td><a href="https://twitter.com/rikumukai?t=v4vnNwKyTTlXcULitfFeKA&s=09&fbclid=IwAR35aXe4OmPmnvG9eAnYw5A1IJS5AAByWXiwlcPFinEyMzSM6ueti0lzFeY">Twitter</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>Riety</td>
+        <td><img src="https://s.isanook.com/ns/0/ui/1695/8479486/205334614_346548756826031_222856568574225023_n.jpg"></td>
+        <td><a href="https://www.facebook.com/rietypahn">FB</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>Bell Warisara</td>
+        <td><img src="https://i.ytimg.com/vi/OhkoGEjHOgo/maxresdefault.jpg"></td>
+        <td><a href="https://www.facebook.com/bellbeawww/">FB</td>
       </tr>
     </table>
   </body>
